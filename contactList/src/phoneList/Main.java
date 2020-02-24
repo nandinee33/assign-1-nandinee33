af23a02c6b1a4139abcce774c7da60180a7a5829
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-02-24 19:07:50 +0530

    making the task object in the main class

--- contactList/src/phoneList/Main.java
+++ contactList/src/phoneList/Main.java
@@ -9,9 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         LinkedList1 linkedList1 = new LinkedList1();
-
-
-
+        Tasks tasks = new Tasks();
 
     }
 }
