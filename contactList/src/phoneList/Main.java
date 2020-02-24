d18077c4b75ed0a9c0f2da8e06fe5e3349402ef5
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-02-24 20:08:20 +0530

    fill the choices in the main class

--- contactList/src/phoneList/Main.java
+++ contactList/src/phoneList/Main.java
@@ -18,6 +18,30 @@ public class Main {
         System.out.println("Press 5 to exit program ");
 
         int choice = Integer.parseInt(scanner.nextLine());
+        if (choice == 1) {
+            tasks.addContact(linkedList1);
+            choice = Integer.parseInt(scanner.nextLine());
+        }
+
+        if (choice == 2) {
+            tasks.viewcontact(linkedList1);
+            choice = Integer.parseInt(scanner.nextLine());
+
+        }
+        if (choice == 3) {
+            tasks.searchContact(linkedList1);
+            choice = Integer.parseInt(scanner.nextLine());
+
+        }
+        if (choice == 4) {
+            tasks.deleteContact(linkedList1);
+            choice = Integer.parseInt(scanner.nextLine());
+
+        }
+        if (choice == 5) {
+            System.out.println("terminate");
+
+        }
 
 
     }
