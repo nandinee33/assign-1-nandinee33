3234ac419d53eeb18fce8d5d8a9b96aa8874b21e
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-02-24 19:38:35 +0530

    make the getNode method in Tasks class

--- contactList/src/phoneList/Tasks.java
+++ contactList/src/phoneList/Tasks.java
@@ -9,5 +9,76 @@ public class Tasks {
     private Node first;
     private Node last;
 
+    public void addFirst(Person person) {
 
+    }
+
+    public void addLast(Person person) {
+
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+
+    public boolean exist(String firstName) {
+        return true;
+    }
+
+
+    public int indexOf(String firstName) {
+        return -1;
+    }
+
+
+    public void removeFirst() {
+
+    }
+
+    public void removeLast() {
+
+    }
+
+    private Node getPreviousNode(Node node) {
+        return null;
+    }
+
+    public void deleteNode(int position) {
+
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void display() {
+
+    }
+
+    public void displayOne() {
+
+    }
+
+    public void getNode(int pos) {
+        var current = first;
+        int index = 0;
+        while (current != null) {
+            if (index == pos) {
+                System.out.println();
+                System.out.println("-----------------------*-------------------------");
+                System.out.println("Name:" + current.getValue().getFirstName() + " " + current.getValue().getLastName());
+                System.out.println("Contacts:" + current.getValue().getContacts().toString());
+                System.out.println("Email:" + current.getValue().getEmail());
+                System.out.println("------------------------*-------------------------");
+                System.out.println();
+                break;
+            }
+            index++;
+            current = current.getNext();
+        }
+
+
+    }
 }
+
