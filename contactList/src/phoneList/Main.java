4835297c16343e9b2f8dd71eb909d5389f42247c
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-02-24 19:05:47 +0530

    taking input from scanner in the main class

--- contactList/src/phoneList/Main.java
+++ contactList/src/phoneList/Main.java
@@ -1,4 +1,14 @@
 package phoneList;
 
+import Helper.LinkedList1;
+
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
 }
