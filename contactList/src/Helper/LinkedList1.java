ed23310f7e7914fb8b1c125a66e6cb2cea432b08
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-02-24 18:58:32 +0530

    taking input from the scanner

--- contactList/src/Helper/LinkedList1.java
+++ contactList/src/Helper/LinkedList1.java
@@ -1,10 +1,11 @@
 package Helper;
 
+import Person.Person;
+import Helper.LinkedList1;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class LinkedList1 {
 
-
-
-
+    Scanner scanner = new Scanner(System.in);
 }
