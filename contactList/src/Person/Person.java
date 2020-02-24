7f0b75d02605e1a99ba90d0ba0857abfe400646a
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-02-24 18:46:16 +0530

    make constrctor in person class

--- contactList/src/Person/Person.java
+++ contactList/src/Person/Person.java
@@ -4,10 +4,15 @@ import java.util.LinkedList;
 
 public class Person {
 
-        private String firstName;
-        private String lastName;
-        private LinkedList<String> contacts;
-        private String email;
+    private String firstName;
+    private String lastName;
+    private LinkedList<String> contacts;
+    private String email;
+
+    public Person(String firstName, String lastName, LinkedList<String> contacts, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contacts = contacts;
+        this.email = email;
     }
-
-
+}
