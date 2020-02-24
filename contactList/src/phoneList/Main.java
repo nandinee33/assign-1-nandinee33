32a4f0f52a526c2e943e59e3db6ff92da4ade403
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-02-24 19:06:46 +0530

    making the linked list object in the main class

--- contactList/src/phoneList/Main.java
+++ contactList/src/phoneList/Main.java
@@ -8,6 +8,9 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        LinkedList1 linkedList1 = new LinkedList1();
+
+
 
 
     }
